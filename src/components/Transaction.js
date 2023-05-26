c64e42d4cf5cc1f260a0eea9ee226ccd1745cc74
guillaume-vandeville <guillaume.vandeville@viacesi.fr>
2023-05-26 03:29:49 +0200

    totaly reshaped the list implementation

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,28 @@
-import React, {useContext} from 'react'
-import { GlobalContext } from '../context/GlobalState'
-
+import React from "react";
 
 export const Transaction = ({ transactions }) => {
-    const { deleteTransaction } = useContext(GlobalContext);
     return (
-        <li>
-            {transactions.text} <span>₱{transactions.amount}</span> <button onClick={() => deleteTransaction(transactions.id)} className="delete-btn">x</button>
-        </li>
-    )
-}
+      <tr>
+        <td>{transactions.id}</td>
+        <td>{transactions.text}</td>
+        <td>{transactions.timezone}</td>
+        <td>{transactions.status}</td>
+        <td>{transactions.date}</td>
+        <td>{transactions.city}</td>
+        <td>{transactions.country}</td>
+        <td>{transactions.transport}</td>
+        <td>{transactions.currency}</td>
+        <td>{transactions.price}</td>
+        <td>{transactions.quantity}</td>
+        <td>{transactions.fname}</td>
+        <td>{transactions.lname}</td>
+        <td>{transactions.email}</td>
+        <td>{transactions.verification}</td>
+        <td>{transactions.platform}</td>
+        <td>{transactions.security}</td>
+        <td>{transactions.contact}</td>
+        <td>{transactions.approval}</td>
+        <td>{transactions.delivered}</td>
+      </tr>
+    );
+  };
