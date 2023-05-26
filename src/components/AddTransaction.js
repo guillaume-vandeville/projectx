ef32c58d2fbfe00671921da9955e294e70842d19
guillaume-vandeville <guillaume.vandeville@viacesi.fr>
2023-05-26 03:30:10 +0200

    fixed add transaction bugs

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,34 +1,41 @@
 import React, {useState, useContext} from 'react'
 import { GlobalContext } from '../context/GlobalState';
 
+
 export const AddTransaction = () => {
   const [text, setText] = useState('');
-  const [amount, setAmount] = useState(0);
-  const { addTransaction } = useContext(GlobalContext);
-  const sentForm = e => {
+  const [price, setPrice] = useState(0);
+  const { transactions, addTransaction } = useContext(GlobalContext);
+
+  // Helper function to generate unique ID
+const getNextId = (transactions) => {
+  const maxId = Math.max(...transactions.map((transaction) => transaction.id));
+  return maxId >= 10 ? maxId + 1 : 11;
+};
+
+  const sentForm = (e) => {
     e.preventDefault();
     const newTransaction = {
       text,
-      amount: +amount,
-      id: Math.floor(Math.random()*123454321),
-    }
+      price: +price,
+      id: getNextId(transactions),
+    };
     addTransaction(newTransaction);
-  }
+  };
 
   return (
     <>
-      <h3>Add new receipt</h3>
+      <h3>Add new receipt (for test)</h3>
       <form onSubmit={sentForm}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e)=> setText(e.target.value)} placeholder="Enter text..." />
         </div>
         <div className="form-control">
-          <label htmlFor="amount"
-            >Amount <br />
-            (negative - expense, positive - income)</label
-          >
-          <input type="number"  value={amount} onChange={(e)=> setAmount(e.target.value)} placeholder="Enter amount..." />
+          <label htmlFor="price"
+            >Price <br />
+          </label>
+          <input type="number"  value={price} onChange={(e)=> setPrice(e.target.value)} placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
       </form>
